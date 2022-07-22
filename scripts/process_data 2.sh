21d1113557d78c754914e181417780cd0636a1a7
Tianjian Li <litianjian_1998@sina.com>
2022-07-22 15:02:02 +0800

    delete duplicate files

--- scripts/process_data 2.sh
+++
@@ -1,4 +0,0 @@
-python process_data.py \
-  --tokenizer-type ChineseSPTokenizer \
-  --tokenizer-model-type /Users/zhengxiaodu/Downloads/mglm-unigram-250k/mglm250k-uni.model \
-  --train-data multilingual
